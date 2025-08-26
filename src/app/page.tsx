fbad35fda3ec9cf3a6455a224d4f6f9a4ee8080b
Ravi Yadav <ravi.y0102@gmail.com>
2025-08-26 15:17:21 +0000

    can you change the color of our website logo to the color of active send

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const AiIcon = () => (
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="h-8 w-8 text-primary"
+      className="h-8 w-8 text-accent"
     >
       <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2Z" />
       <path d="M12 18a6 6 0 1 0 0-12" />
